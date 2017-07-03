174d6e58b2a5adf9f403b2b9c69be1a95d736a15
anistar sung <cfsung@yahoo-inc.com>
2017-07-03 10:39:31 +0800

    Fixing property setting error

--- AppDevKit/AppDelegate.h
+++ AppDevKit/AppDelegate.h
@@ -13,7 +13,7 @@
 @interface AppDelegate : UIResponder <UIApplicationDelegate>
 
 @property (strong, nonatomic) UIWindow *window;
-@property (assign, nonnull) BOOL restrictRotation;
+@property (assign, nonatomic) BOOL restrictRotation;
 
 @end
 
