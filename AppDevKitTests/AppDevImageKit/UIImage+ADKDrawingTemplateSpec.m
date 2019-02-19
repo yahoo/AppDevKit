a99633338221a8242d9353882c3a72eee1ccfaab
anistar sung <cfsung@yahoo-inc.com>
2019-02-19 15:03:50 +0800

    Adding BSD license statment in header file

--- AppDevKitTests/AppDevImageKit/UIImage+ADKDrawingTemplateSpec.m
+++ AppDevKitTests/AppDevImageKit/UIImage+ADKDrawingTemplateSpec.m
@@ -4,6 +4,8 @@
 //
 //  Created by  Chih Feng Sung on 2/19/19.
 //  Copyright © 2019 Yahoo. All rights reserved.
+//  Licensed under the terms of the BSD License.
+//  Please see the LICENSE file in the project root for terms.
 //
 
 #import <Kiwi.h>
